c3a30559086095de929e88c4eaae48072311120f
Bojan Raic <bojanraic@outlook.com>
2024-02-11 19:11:23 +0100

    Fixing stuff
    Fixing stuff

--- charts/bwsm-eso-provider/README.md
+++ charts/bwsm-eso-provider/README.md
@@ -1,6 +1,6 @@
 # bwsm-eso-provider
 
-![Version: 0.0.3](https://img.shields.io/badge/Version-0.0.3-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: 0.0.3](https://img.shields.io/badge/AppVersion-0.0.3-informational?style=flat-square)
+![Version: 0.0.6](https://img.shields.io/badge/Version-0.0.6-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: 0.0.6](https://img.shields.io/badge/AppVersion-0.0.6-informational?style=flat-square)
 
 Helm chart to use Bitwarden Secrets Manaager (BWSM) as a Provider for External Secrets Operator (ESO)
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import http from "http";
 import express from "express";
-import fs from 'fs/promises';
+import fs from "fs/promises";
 import { initialize } from "@oas-tools/core";
 import constants from "./util/constants.js";
 
-async function readAndParseConfig(filePath) {
+async function readAndParse(filePath) {
     try {
         const configFileContent = await fs.readFile(filePath, 'utf8');
         return JSON.parse(configFileContent);
@@ -16,20 +16,30 @@ async function readAndParseConfig(filePath) {
 
 const deploy = async () => {
     try {
-        const defaultConfigPath = constants.defaults.CONFIG_FILE_PATH;
+        // application name and version
+        const packageJSON = await readAndParse('./package.json');
+        const appName = packageJSON.name; 
+        const appVersion = packageJSON.version;
 
+        // application configuration
+        const defaultConfigPath = constants.defaults.CONFIG_FILE_PATH;
         const configPath = process.env.CONFIG_FILE_PATH || defaultConfigPath;
-        const config = await readAndParseConfig(configPath);
+        const config = await readAndParse(configPath);
+        const serverPort = config?.server?.port || constants.defaults.SERVER_PORT;
+        const appMessage = config.server.defaultMessage || constants.defaults.DEFAULT_MESSAGE;
         config.oasFile = constants.defaults.OAS_FILE_PATH;
-        const serverPort = config?.server?.port || constants.defaults.SERVER_PORT
+
+        // application setup
         const app = express();
-        app.use(express.json(config?.server?.express?.json?.config || constants.defaults.EXPRESS_JSON_CONFIG ));
+        app.use(express.json(config?.server?.express?.json?.config || constants.defaults.EXPRESS_JSON_CONFIG));
         app.get('/', (req, res) => {
-            res.send({"url": req.originalUrl, "message": config.server.defaultMessage || constants.defaults.DEFAULT_MESSAGE});
+            res.send({ "name": appName, "version": appVersion, "url": req.originalUrl, "message": appMessage });
         });
+
+        // application initialization
         initialize(app, config).then(() => {
             http.createServer(app).listen(serverPort, () => {
-                console.log(`\nApp running at http://localhost:${serverPort}`);
+                console.log(`\n[${appName} v${appVersion}] Running at http://localhost:${serverPort}`);
                 console.log(`________________________________________________________________`);
                 if (!config?.middleware?.swagger?.disable) {
                     console.log(`API docs (Swagger UI) available on http://localhost:${serverPort}/docs`);
